0e1a57b457962ed6938c7224b5473d0aab142e58
TatianaSteshakova <65027776+TatianaSteshakova@users.noreply.github.com>
2023-03-12 22:09:42 +0300

    Add files via upload

---
+++ Homework3.5.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(0, "rus");
+    int i = 0;
+    while (i < 9)
+    {
+        cout << "Hello" << endl;
+        i++;
+    }
+    cout << endl;
+    i = 0;
+    while (i < 15)
+    {
+        cout << i << endl;
+        i++;
+    }
+    cout << endl;
+    i = 5;
+    int sum = 0;
+    while (i <= 15)
+    {
+        sum += i;
+        cout << sum << endl;
+        i++;
+    }
+}
